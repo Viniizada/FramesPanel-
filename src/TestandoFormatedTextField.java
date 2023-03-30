b4764f1a0d98d8f43a42abb2a6f8f72050cc4c0e
Viniizada <127901612+Viniizada@users.noreply.github.com>
2023-03-30 20:48:48 -0300

    Add files via upload

---
+++ src/TestandoFormatedTextField.java
@@ -0,0 +1,18 @@
+import java.awt.Container;
+import java.text.ParseException;
+
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.text.MaskFormatter;
+
+public class TestandoJFormattedTextField extends framesPanels {
+    private static final long serialVersionUID = 1l;
+
+    public static void main(String[] args) {
+        TestandoJFormattedTextField field = new TestandoJFormattedTextField();
+        field.testaJFormattedTextField();
+    }
+
+
+}
